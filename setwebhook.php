f4a6af6ebaba5786e75c6209271301251d4e27dd
Arkadiy14 <47496737+Arkadiy14@users.noreply.github.com>
2021-04-01 11:30:48 +0300

    Make it more understandable

--- setwebhook.php
+++ setwebhook.php
@@ -3,7 +3,7 @@ const TOKEN = 'YOUR TOKEN';
 $method = 'setWebhook';
 $url = 'https://api.telegram.org/bot'.TOKEN.'/'.$method;
 $options = [
-	'url' => 'https://timebottelegram.herokuapp.com/'
+	'url' => 'YOUR URL'
 ];
 
 $response = file_get_contents($url.'?'.http_build_query($options));
